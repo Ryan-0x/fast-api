6a57cb273da497961437e125caeab4ba1fbfc559
Ryan0x <chiayi520@gmail.com>
2024-12-16 22:45:13 +0800

    update

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
-engine = create_engine(os.getenv("DATABASE_URL"),echo=True)
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://basfAdmin:BaSfAdm1n-!xx@postgres:5433/postgres")
+engine = create_engine(DATABASE_URL,echo=True)
 
 def init_db():
     with Session(engine) as session:
--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vue.js default port
+    allow_origins=["http://localhost:8080"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
